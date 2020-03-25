dab547dcf9cfbba80c5d959d06a765b97916f5f9
cade.gardner <d00299297@dmail.dixie.edu>
2020-03-25 13:57:03 -0600

    styling besides svg

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
+        titleStatus: "Busy",
+        totalVisitors: "9,000",
+        parkingStat: "3%",
+        eastEntranceStat: "503",
+        southEntranceStat: "1,203",
+        yesterdayTitleStatus: "Busy",
+        yesterdayZionTotal: "7310",
+        yesterdayCanyonTotal: "6256",
+        SEVehicles: "1,203",
+        SEPeople: "2,600",
+        EVehicles: "503",
+        EPeople: "1,200",
+        riverVehicles: "",
+        riverPeople: "93",
+        kolobVehicles: "500",
+        kolobPeople: "1,300",
     },
 
     methods: {
