6dfb490d5a6b88e6dba94cc023a54d15b19bbcaf
cade.gardner <d00299297@dmail.dixie.edu>
2020-03-25 15:24:09 -0600

    svg almost working

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,32 @@
+ /*jshint esversion: 6 */
+ 
+ // Start of functions for the SVG circle
+ function getRandomIntInclusive(min, max) {
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+ }
+   
+ function setStop(id, radius, stop){
+     var c = document.getElementById(id);
+     c.className = "background";
+     var stopVal = Math.PI * radius * 2 * (stop/10);
+     c.setAttribute("stroke-dasharray", stopVal + ", 3000");
+     c.setAttribute("stroke-dashoffset", stopVal);
+     c.className = "overlayLine";
+ }
+   
+ function randomStops(){
+     setStop("line1", 6, getRandomIntInclusive(1, 10));
+     setStop("line2", 9, getRandomIntInclusive(1, 10));
+     setStop("line3", 12, getRandomIntInclusive(1, 10));
+ }
+ randomStops();
+
 var app = new Vue({
     el: '#app',
     data: {
+        currentTemp: "75",
         titleStatus: "Busy",
         totalVisitors: "9,000",
         parkingStat: "3%",
