8c668a128703e3d88f7ff3137b9963ef1d92fe5c
treydinwinward <treydinw@gmail.com>
2020-03-26 13:18:42 -0600

    SVG commented out; added Entrances dropdown menu

--- dashboard.js
+++ dashboard.js
@@ -1,27 +1,27 @@
  /*jshint esversion: 6 */
  
  // Start of functions for the SVG circle
- function getRandomIntInclusive(min, max) {
-     min = Math.ceil(min);
-     max = Math.floor(max);
-     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
- }
+//  function getRandomIntInclusive(min, max) {
+//      min = Math.ceil(min);
+//      max = Math.floor(max);
+//      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+//  }
    
- function setStop(id, radius, stop){
-     var c = document.getElementById(id);
-     c.className = "background";
-     var stopVal = Math.PI * radius * 2 * (stop/10);
-     c.setAttribute("stroke-dasharray", stopVal + ", 3000");
-     c.setAttribute("stroke-dashoffset", stopVal);
-     c.className = "overlayLine";
- }
+//  function setStop(id, radius, stop){
+//      var c = document.getElementById(id);
+//      c.className = "background";
+//      var stopVal = Math.PI * radius * 2 * (stop/10);
+//      c.setAttribute("stroke-dasharray", stopVal + ", 3000");
+//      c.setAttribute("stroke-dashoffset", stopVal);
+//      c.className = "overlayLine";
+//  }
    
- function randomStops(){
-     setStop("line1", 6, getRandomIntInclusive(1, 10));
-     setStop("line2", 9, getRandomIntInclusive(1, 10));
-     setStop("line3", 12, getRandomIntInclusive(1, 10));
- }
- randomStops();
+//  function randomStops(){
+//      setStop("line1", 6, getRandomIntInclusive(1, 10));
+//      setStop("line2", 9, getRandomIntInclusive(1, 10));
+//      setStop("line3", 12, getRandomIntInclusive(1, 10));
+//  }
+//  randomStops();
 
 var app = new Vue({
     el: '#app',
@@ -44,11 +44,12 @@ var app = new Vue({
         riverPeople: "93",
         kolobVehicles: "500",
         kolobPeople: "1,300",
+        MainPage: 'Home', // Home, Parking, Entrances 
+        EntrancesPage: ['SouthEast', 'East', 'River', 'Kolob'],
+
     },
 
     methods: {
-        test: function() {
-            console.log('pushed');
-        }
+        
     },
 });
